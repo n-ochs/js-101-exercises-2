cd8c6ffff792b0e4f63f851ea84fd8966f52ba0c
Nicholas Ochs <nick.ochs@yahoo.com>
2020-08-28 20:39:53 -0400

    initial commit

---
+++ exercises/level1.js
@@ -0,0 +1,41 @@
+function lvl1exercise1 () {
+  // Declare a variable without instantiating it and return it!
+  var thisVariable;
+  return thisVariable;
+}
+
+function lvl1exercise2 () {
+  // Declare and instantiate a number and return it
+  var number1 = 1;
+  return number1;
+}
+
+function lvl1exercise3 () {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+  var numberFloat = 4.7;
+  return numberFloat;
+}
+
+function lvl1exercise4 () {
+  // Declare and instantiate a string "Hello World!" and return it
+  var someString = 'Hello World!';
+  return someString;
+}
+
+function lvl1exercise5 () {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  let thisArray = ['Hello World!', 4];
+  return thisArray;
+}
+
+function lvl1exercise6 () {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  const thisObject = {key1: 'Hello World!', key2: 4};
+  return thisObject;
+}
+
+function lvl1exercise7 () {
+  // Declare and instantiate a boolean value 'false' and return it
+  const boolean1 = false;
+  return boolean1;
+}
